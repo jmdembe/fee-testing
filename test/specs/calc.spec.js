a357e2d75d6975b0fa36d89efd00305472d01b68
Jessica Dembe <jmdembe@gmail.com>
2016-11-07 15:13:49 -0500

    Created test to know if sum accepts a number as an argument

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -5,13 +5,18 @@
     var expect = chai.expect;
     window.calc = window.calc || {};
 
-    describe('my first test for calculator', function () {
+    describe('test for calculator', function () {
         it('should know that one equals one', function () {
             expect(1).to.equal(1);
         });
 
         it('should know that sum is a function', function (){
             expect(window.calc.sum).to.be.a('function');
+        });
+
+        it('should know that sum accepts a number as an argument', function () {
+            var result = window.calc.sum([4,5,6])
+            expect(result).to.equal(15);
         });
 
     });
