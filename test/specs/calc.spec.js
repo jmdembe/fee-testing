32413a02806e113cc073013e3780e4197c5c741f
Jessica Dembe <jmdembe@gmail.com>
2016-11-07 14:24:29 -0500

    Renamed calc.js to calc.spec.js

---
+++ test/specs/calc.spec.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+
+    // TODO: Your tests go here!
+    var expect = chai.expect;
+
+    describe('my first test for calculator', function () {
+        it('should know that one equals one', function () {
+            expect(1).to.equal(1);
+        });
+    });
+
+
+
+})();
